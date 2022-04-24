dfdfd60ceaffa5a6940033cd8aace2cfcc2f3b7e
Julius Sladek <julo.sladek@gmail.com>
2022-04-24 16:07:37 +0200

    Add project files.

---
+++ Farm_Simulator/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+	
+	cout << "hello world - Farm Simulator";
+
+	return 0;
+}
---
+++ PI_search/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+
+	cout << "hello world - PI search";
+
+	return 0;
+}
